1463a7497797140f717831737ca4b4db65be51b6
Jorge Benavides Nati <jbenavides@sistecredito.com>
2023-04-29 11:08:06 -0500

    guardo mis cambios

---
+++ Piloto.py
@@ -0,0 +1,7 @@
+class Piloto: 
+    def __init__(self):
+        self.__nombre = None
+        self.__fechaNaciemiento = None
+        self.__salarioAnual = None
+        self.__pais = None
+        
